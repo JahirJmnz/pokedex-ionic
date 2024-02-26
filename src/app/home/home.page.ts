6a419f43cb7f6ea3320df0a3ee300ab45d2d5102
Jahir Jimenez <jahirjimenez@MacBook-Air-de-Jahir.local>
2024-02-26 14:20:07 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { IonInfiniteScroll } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+// 2. Implementar OnInit
+export class HomePage implements OnInit{
+
+  // 4. Declarar que inicie la lista en el elemento 0
+  offset = 0;
+
+  // P2 1. Definir array de pokemos
+  // NOTA: Se añade que es de tipo "any[]", sino da error
+  pokemon: any[] = [];
+
+  // P4 1. Utilizar IonInfiniteScroll de otra forma con ViewChild, hay que poner undefined infinite
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll | undefined;
+
+  // 1. Inyectar el servicio que se creó en pokemon service ts (PokemonService)
+  constructor(private PokemonService: PokemonService) {}
+
+  // 3. Declarar el OnInit
+  ngOnInit() {
+      this.loadPokemon();
+  }
+
+  // 5. Cargar la lista de pokemons
+  // P3 1. Añadir loadMore para el inffinity scroll
+  // Declarar el tipo de valor "any", sino da error
+  loadPokemon(loadMore = false, event?: any) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+    this.PokemonService.getPokemon(this.offset).subscribe(res => {
+      // 6. Mostrar la variable res como resultado
+      console.log('result: ', res);
+      this.pokemon = [...this.pokemon, ...res];
+      // Aquí sigue siendo P3
+
+      // P2 2. Le decimos que el arreglo de pokemon es igual res
+      // P3 Pase el this.pokemon = res al if
+      // Lo sustitui por "this.pokemon = [...this.pokemon, ...res];"
+
+      if (event) {
+        event.target.complete();
+      }
+
+      // P4 2. De esta forma le decimos al IS que cargue máximo 150, o sea 125 + 25 que ya tenemos
+      // Añadir && this.infinite para inicializar infinite
+      if (this.offset == 125 && this.infinite) {
+        this.infinite.disabled = true;
+      }
+    });
+  }
+
+  // P5 1. Añadir funcionalidad a search
+  onSearchChange(e: any) {
+    let value = e.detail.value;
+
+    if (value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.PokemonService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    });
+
+  }
 
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,76 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+// 5. NOTA: Se tuvo que añadir esta interface porque el map no reconoce 'results', entonces
+// se declaró como tipo any
+// Definir una interfaz para el formato de los datos de respuesta de la API
+interface PokemonApiResponse {
+  results: any[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  // 2. Añadir API de pokeapi
+  baseUrl = 'https://pokeapi.co/api/v2';
+  imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+  constructor(private http: HttpClient) { } // 1. Inyectar http
+
+  // 3. Obtener lista de pokemons - Iniciar en el pokemon número 0
+  getPokemon(offset = 0) {
+
+    // 4. Retornar el baseUrl (lista de pokemones) y limitarla a 25 elementos
+    // Se añade map para obtener una mejor estructura del arreglo de la lista
+    return this.http.get<PokemonApiResponse>(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(
+      map(result => {
+        return result.results;
+      }),
+      // 7. Se retorna la imagen y se añade el index + el offset (o sea la posición 0 de la
+      // lista + 1)
+      // NOTA: Es por eso que el arreglo que regresa tiene image, name, pokeIndex y url
+      map(pokemons => {
+        return pokemons.map((poke, index) => {
+          poke.image = this.getPokeImage(index + offset + 1);
+          poke.pokeIndex = offset + index + 1;
+          return poke;
+        });
+      })
+    )
+  }
+
+  // 6. Función para obtener la imagen en base a imageUrl que se definió en el paso 2
+  // NOTA: en "(index)" se debe específicar el tipo de dato para que no haya problema,
+  // en este caso le puse number "(index: number)"
+  getPokeImage(index: number) {
+    return `${this.imageUrl}${index}.png`;
+  }
+
+  findPokemon(search: any): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${search}`).pipe(
+      map((pokemon: any) => {
+        pokemon['image'] = this.getPokeImage(pokemon['id']);
+        pokemon['pokeIndex'] = pokemon['id'];
+        return pokemon;
+      })
+    )
+  }
+
+  getPokeDetails(index: any): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`).pipe(
+      map(poke => {
+        let sprites = Object.keys(poke['sprites']);
+        poke['images'] = sprites
+          .map(spriteKey => poke['sprites'][spriteKey])
+          .filter(img => img);
+        return poke;
+      })
+    );
+
+
+  }
+}
