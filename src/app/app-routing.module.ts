d421649071243e0a8311a55e38e16747cc549eb1
Jahir Jimenez <jahirjimenez@MacBook-Air-de-Jahir.local>
2024-03-21 09:04:04 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home/:index',
     loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
